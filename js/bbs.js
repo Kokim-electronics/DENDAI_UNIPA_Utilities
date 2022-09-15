77280ce47b855274580ae3b3e3036a418664b1f9
kk_electro_0303 <21eh089@ms.dendai.ac.jp>
2022-09-15 20:20:34 +0900

    v2.1.1-Light_upload

--- js/bbs.js
+++
@@ -1,4 +0,0 @@
-﻿var midoku = document.getElementsByClassName('ui-state-default ui-corner-top')[5];
-if (midoku != undefined) {
-    midoku.click();
-}
--- js/options.js
+++ js/options.js
@@ -4,28 +4,8 @@ function saveurl() {
     var value_id = $('#uido_id').val();
     var value_pass = $('#uido_pass').val();
 
-    var gakki = new Array(3);
-    gakki[1] = document.getElementById("rdo_gakki1").checked;
-    gakki[2] = document.getElementById("rdo_gakki2").checked;
-
-    var value_gakki = 1;
-    for (var n = 1; n <= 3; n++) {
-        if (gakki[n]) {
-            value_gakki = n;
-        }
-    }
-
-    //console.log($("#kamokuary").val().length);
-
-    var arr_kamoku
-    if ($("#kamokuary").val().length == 0) {
-        arr_kamoku = ""
-    } else {
-        arr_kamoku = $("#kamokuary").val().split(',');
-    }
-
     $("#resultlb").text('設定が成功しました．');
-    chrome.storage.local.set({ uido_id: value_id, uido_pass: value_pass, gakki: value_gakki, kamoku: arr_kamoku });
+    chrome.storage.local.set({ uido_id: value_id, uido_pass: value_pass });
 }
 
 $('#confirmbtn').on('click', saveurl);
@@ -34,6 +14,4 @@ $('#confirmbtn').on('click', saveurl);
 chrome.storage.local.get(function (item) {
     $("#uido_id").val(item.uido_id);
     $("#uido_pass").val(item.uido_pass);
-    document.querySelector("input[id=rdo_gakki" + item.gakki + "]").checked = true;
-    $("#kamokuary").val(item.kamoku)
 });
