46f1576a0b902a0acbffb3b5e389b60d3ccd8695
kk_electro_0303 <81867693+Kokim-electronics@users.noreply.github.com>
2023-04-19 11:28:33 +0900

    v3.0.1_update

--- js/autologin.js
+++ js/autologin.js
@@ -1,5 +1,22 @@
-chrome.storage.local.get(function (item) {
-    document.getElementById("loginForm:userId").value = item.uido_id;
-    document.getElementById("loginForm:password").value = item.uido_pass;
-    document.getElementById("loginForm:loginButton").click();
+﻿chrome.storage.local.get(function (item) {
+    if (document.getElementById("loginForm:userId") != undefined) {
+        document.getElementById("loginForm:userId").value = item.uido_id;
+        document.getElementById("loginForm:password").value = item.uido_pass;
+        document.getElementById("loginForm:loginButton").click();
+    }
 });
+
+if (document.getElementsByClassName("innerInfo")[0].innerText != undefined) {
+    if (document.getElementsByClassName("innerInfo")[0].innerText.startsWith('長時間操作が行われなかったため')){
+        location.href = 'https://portal.sa.dendai.ac.jp/';
+    }
+    if (document.getElementsByClassName("innerInfo")[0].innerText.startsWith("別の画面で操作されたため")){
+        location.href = 'https://portal.sa.dendai.ac.jp/';
+    }
+}
+
+
+if (document.querySelectorAll("[control-id=ControlID-1]")[0] != undefined) {
+    document.querySelectorAll("[control-id=ControlID-1]")[0].click
+
+}
--- js/options.js
+++ js/options.js
@@ -4,8 +4,9 @@ function saveurl() {
     var value_id = $('#uido_id').val();
     var value_pass = $('#uido_pass').val();
 
+
     $("#resultlb").text('設定が成功しました．');
-    chrome.storage.local.set({ uido_id: value_id, uido_pass: value_pass });
+    chrome.storage.local.set({ uido_id: value_id, uido_pass: value_pass});
 }
 
 $('#confirmbtn').on('click', saveurl);
